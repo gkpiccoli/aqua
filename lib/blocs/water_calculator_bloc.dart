24c37f9bad15a77c5e6f0e7d5253be02da6d2c4a
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-09-02 22:19:21 -0300

    📦 NEW: Calculadora de água

---
+++ lib/blocs/water_calculator_bloc.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+class WaterCalculatorBloc {
+  // StreamController para gerenciar o estado da quantidade de água
+  final _waterAmountController = StreamController<double>.broadcast();
+
+  // Stream e Sink
+  Stream<double> get waterAmount => _waterAmountController.stream;
+  Sink<double> get _setWaterAmount => _waterAmountController.sink;
+
+  // Função para calcular a quantidade de água
+  void calculateWaterAmount(double weight, double height, int age) {
+    // Lógica para calcular a quantidade de água
+    // Esta é apenas uma fórmula fictícia
+    double amount = (weight + height) / age * 10;
+
+    // Atualizar o estado
+    _setWaterAmount.add(amount);
+  }
+
+  // Função para descartar o StreamController
+  void dispose() {
+    _waterAmountController.close();
+  }
+}
